c49397ec04cb46e86fb6943111abbe5ffb460b87
anaalvarezm <78774026+anaalvarezm@users.noreply.github.com>
2023-04-30 23:56:24 -0400

    fix?

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include("polls.urls")),  # added this line to include polls app for the empty path
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
